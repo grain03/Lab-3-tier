5a2a6ecb90ef361ea57bdb659aab8ea54e0c44ab
Reda Grain <grain.reda.solicode@gmail.com>
2023-10-10 13:39:30 +0100

    Debugged

--- loader.php
+++ loader.php
@@ -2,8 +2,8 @@
 define('Root', dirname(__FILE__));
     error_reporting(E_ALL);
 
-    require_once Root . './Application/DatabaseConnection.php';
-    require_once Root . './Application/DAL/StudentDAO.php';
-    require_once Root . './Application/BLL/StudentBLO.php';
-    require_once Root . './Application/Entities/Student.php';
+    require_once './Application/DatabaseConnection.php';
+    require_once './Application/DAL/StudentDAO.php';
+    require_once './Application/BLL/StudentBLO.php';
+    require_once './Application/Entities/Student.php';
 
